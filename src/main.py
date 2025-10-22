67d3db7e55630f37105638bf0abe1c2ac75e9c42
joshbrod <joshbrod@umich.edu>
2025-10-22 01:10:33 -0400

    Added benchmark file format

--- src/main.py
+++ src/main.py
@@ -5,44 +5,64 @@ Walks through each kernel in the benchmark to:
 - Initiate a Validation loop until correctly generated kernel  
 """
 
+import sys
+import json
+
+import os
+os.makedirs("generated_kernels", exist_ok=True)
+
 import src.monitor
 import src.generator
 import src.verifier
 
 MAX_ATTEMPTS = 5
 
-def generate_kernel(inputs: list[str], code: str):
+def generate_kernel(program: dict[str], counter: int):
     """Monitors operations, generates corresponding kernel, and validates correctness for each sample of benchmark.
 
     Args:
         inputs (list[str]): List of inputs that the sample takes (values, size, etc.)
         code (str): Literal PyTorch code to be executed
     """
- 
-    output, aten_output, kernel_output = src.monitor.extract_op_details(inputs, code)
+
+    output, aten_output, kernel_output = src.monitor.extract_op_details(program)
     
     op_details = f"aten output:\n{aten_output}\n\n\nkernel output:\n{kernel_output}"
-    generated_code = src.generator.ollama_generator(op_details)
+
+    generated_code = src.generator.gemini_generator(op_details)
     
     for i in range(MAX_ATTEMPTS):
-        is_valid, feedback = src.verifier.verify_cuda(generated_code, output)
-
+        is_valid, feedback = src.verifier.verify_cuda(generated_code, str(output))
+        
+        with open(f"generated_kernels/{os.path.basename(sys.argv[1])}_{counter}_iter{i}.txt", "w") as f:
+            f.write(f"feedback {i}: {feedback}\ncode {i}: {generated_code}\n\n\n")
+            
         if is_valid:
-            with open("final_kernel.py", "w") as f:
+            with open(f"generated_kernels/{sys.argv[1]}_{counter}.cu", "w") as f:
                 f.write(generated_code)
             return
 
-        generated_code = src.generator.ollama_fixer(generated_code, feedback, op_details)
+        generated_code = src.generator.gemini_fixer(generated_code, feedback, op_details)
 
-
+    with open(f"generated_kernels/{sys.argv[1]}_{counter}.cu", "w") as f:
+        f.write(generated_code)
 
 def main():
     """Loops through each sample in benchmark."""
     
-    benchmark = [] # TODO
-    for inputs, code in benchmark:
-        generate_kernel(inputs, code)
+    benchmarks = []
+    with open(sys.argv[1], "r") as f:
+        benchmarks = json.load(f)
 
+    counter = 0
+    for program in benchmarks:
+        generate_kernel(program, counter)
+        counter += 1
 
 if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Missing benchmark file `python3 src/main.py <benchmark file>")
+        sys.exit(1)
+    
+    
     main()
--- src/monitor.py
+++ src/monitor.py
@@ -13,6 +13,8 @@ def handle_trace(prof):
     Display profiled events grouped by high-level ATen operators, 
     with the associated CUDA kernels underneath, preserving order.
     """
+    global kernel_output
+    global aten_output
 
     current_op = None
     for event in prof.events():
@@ -40,7 +42,7 @@ def handle_trace(prof):
                 kernel_output += f"  Device Time (ms): {event.self_device_time_total / 1000:.3f}\n"
 
 
-def extract_op_details(inputs: list[str], code: str):
+def extract_op_details(program: list[str]):
     """Extracts relevant information of sample PyTorch code including: 
     - High level kernel PyTorch operators
     - Names of PyTorch’s internal CUDA kernel/cuBLAS kernels/etc.
@@ -56,12 +58,23 @@ def extract_op_details(inputs: list[str], code: str):
         str: Formatted string of low level kernel names of sample
     """
 
-    a = torch.randn(2048, 2048, device="cuda")
-    b = torch.randn(2048, 2048, device="cuda")
-
+    # Set state to be deterministic
+    torch.cuda.manual_seed(100)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    
     print("\n[Main Thread] Starting profiled execution...")
+    
+    context = {"torch": torch}
 
-    # TODO: Define schedule parameters explicitly 
+    # execute definitions
+    for definition in program["definitions"]:
+        var = definition["variable"]
+        val = definition["value"]
+        exec(f"{var} = {val}", context)
+
+    print("\n[Main Thread] Executed definitions...")
+    
     wait_steps = 1
     warmup_steps = 1
     active_steps = 2
@@ -83,14 +96,17 @@ def extract_op_details(inputs: list[str], code: str):
         schedule=schedule,
         on_trace_ready=handle_trace,
         record_shapes=True
-    ) as p:
+    ) as p:    
         # --- Calculate the total number of steps for the loop ---
         total_steps = (wait_steps + warmup_steps + active_steps) * repeat_cycles
         for i in range(total_steps):
-            c = torch.matmul(a, b)
-            d = torch.sin(c)
-            e = torch.sin(d)
+            
+            for op in program["operations"]:
+                assignment = op["assignment"]
+                operation = op["operation"]
+                exec(f"{assignment} = {operation}", context)
             p.step()
+
 
         # TODO:
         # I put this here to guarantee profiler is done before continuing
@@ -99,6 +115,6 @@ def extract_op_details(inputs: list[str], code: str):
         handle_trace(p)
 
         # TODO: Get output variable?
-        output = -1
+        output = context.get("output", None)
 
     return output, aten_output, kernel_output
